55726a941cc7d850c4212e7f8ad88cebbcc1d26a
ronanzhan <ronan.zhan@gmail.com>
2025-02-04 06:30:13 +0800

    add quantlib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <fmt/core.h>
+#include <ql/math/array.hpp>
+
+
+using namespace std;
+using namespace QuantLib;
 
 int main() {
     fmt::println("Hello, World!");
+
+
     return EXIT_SUCCESS;
 }
