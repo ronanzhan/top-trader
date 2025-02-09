b95e61b3b3a05f72a1c95b63f12492b68b3d1da1
ronanzhan <ronan.zhan@gmail.com>
2025-02-09 08:41:41 +0800

    integrate user interface with gtkmm

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
-#include <fmt/core.h>
-#include <ql/math/array.hpp>
+#include <gtkmm.h>
 
 
-using namespace std;
-using namespace QuantLib;
+class MyWindow : public Gtk::Window {
+public:
+    MyWindow();
+};
 
-int main() {
-    fmt::println("Hello, World!");
+MyWindow::MyWindow() {
+    set_title("Top Trader");
+    set_default_size(600, 400);
+}
+
+
+int initUI(int argc, char *argv[]) {
+    auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+    return app->make_window_and_run<MyWindow>(argc, argv);
+}
+
+int init(int argc, char *argv[]) {
+    initUI(argc, argv);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    init(argc, argv);
 
 
     return EXIT_SUCCESS;
